05b9efc2b952bfba69a9d5f3f62e5c43968f88f0
Sebastião Ferreira <sebastorama@gmail.com>
2012-03-27 20:01:32 -0300

    Adding one simples pascal example

---
+++ examples/lexical/correct1.pas
@@ -0,0 +1,13 @@
+program Lesson1_Program1;   
+begin    
+  var_1: integer;
+  var_2: integer;
+  var_3: integer;
+
+  var_1 := 1;
+  var_2 := 2;
+  {var_1 := 3;
+  var_2 := 4;}
+  var_3 := var_1 + var_2;
+  read;   
+end.
