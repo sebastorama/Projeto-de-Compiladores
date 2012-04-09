b5e42f3ef52204cfe87b648ff02870b3b8e9f7a2
Sebastião Ferreira <sebastorama@gmail.com>
2012-04-09 10:58:08 -0300

    Adding some more examples

---
+++ examples/lexical/incorrect1.pas
@@ -0,0 +1,19 @@
+program Teste2;
+procedure fatorial(i : integer, res : integer);
+aux :integer;
+begin
+	aux := 1;
+	while i > 0 do
+	begin
+		aux := aux * i; 
+		i := i-1;
+	end;
+	res := aux;
+end;
+%
+begin
+	res: integer;
+
+
+	fatorial(5, res);
+end.
