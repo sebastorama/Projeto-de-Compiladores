6acdfc4475ad09cba1326a9bfe93d0ecdb05db7d
Sebastião Ferreira <sebastorama@gmail.com>
2012-05-29 20:39:43 -0300

    Added SymbolTable interface for future implementation

---
+++ SymbolTable.java
@@ -0,0 +1,17 @@
+public class SymbolTable {
+	public boolean isDeclared(String symbol, int current_level) {
+		return false;
+	}
+	public void insert(String symbol, boolean ref, int current_level) {
+
+	}
+
+	public boolean search(String symbol, boolean ref) {
+
+		return false;
+	}
+
+	public void destroy(int current_level) {
+
+	}
+}
